293eaaacd6372adfbd4e80edebcd3c8821309881
krushna Durole <krushnadurole2000ad@gmail.com>
2023-02-28 22:08:52 +0530

    Database is secured

--- config/database.js
+++ config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
-const MONGO_URI = "mongodb+srv://krish_durole:2Qj82o3XrpKotcJK@cluster0.qmv0wcd.mongodb.net/FlipKart?retryWrites=true&w=majority";
-// const MONGO_URI = process.env.MONGO_URI;
 
 const connectDatabase = () => {
+    const MONGO_URI = process.env.MONGOURI;
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Mongoose Connected");
